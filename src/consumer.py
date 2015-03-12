dd27a42ffc52995506c9c707e377eebff6075c8a
Povilas Balciunas <balciunas90@gmail.com>
2015-03-12 12:28:06 +0200

    Converted tabs to spaces in python scripts.

--- src/consumer.py
+++ src/consumer.py
@@ -4,24 +4,24 @@ import pika
 
 
 def main():
-	consume_message()
+    consume_message()
 
 
 def consume_message():
-	connection = pika.BlockingConnection(
-		pika.ConnectionParameters("localhost"))
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters("localhost"))
 
-	channel = connection.channel()
-	channel.queue_declare(queue = "hello")
-	channel.basic_consume(on_msg_receive, queue = "hello", no_ack = True)
-	channel.start_consuming()
+    channel = connection.channel()
+    channel.queue_declare(queue = "hello")
+    channel.basic_consume(on_msg_receive, queue = "hello", no_ack = True)
+    channel.start_consuming()
 
-	connection.close()
+    connection.close()
 
 
 def on_msg_receive(channel, method, properties, body):
-	print("Received %r" % body)
+    print("Received %r" % body)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/producer.py
+++ src/producer.py
@@ -4,20 +4,20 @@ import pika
 
 
 def main():
-	produce_message("Hello world!")
+    produce_message("Hello world!")
 
 
 def produce_message(msg):
-	connection = pika.BlockingConnection(
-		pika.ConnectionParameters("localhost"))
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters("localhost"))
 
-	channel = connection.channel()
-	channel.queue_declare(queue = "hello")
-	channel.basic_publish(exchange = "", routing_key = "hello",
-		body = msg)
+    channel = connection.channel()
+    channel.queue_declare(queue = "hello")
+    channel.basic_publish(exchange = "", routing_key = "hello",
+        body = msg)
 
-	connection.close()
+    connection.close()
 
 
 if __name__ == "__main__":
-	main()
+    main()
