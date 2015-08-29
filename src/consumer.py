db6d80eefb42740e9d124de74fcf666715676dd9
Povilas Balciunas <balciunas90@gmail.com>
2015-08-29 10:49:05 +0300

    Create dedicated exchange

--- src/consumer.py
+++ src/consumer.py
@@ -9,11 +9,25 @@ def main():
 
 def consume_message():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters("localhost"))
+        pika.ConnectionParameters("192.168.1.222"))
 
     channel = connection.channel()
-    channel.queue_declare(queue = "hello")
-    channel.basic_consume(on_msg_receive, queue = "hello", no_ack = True)
+
+    channel.exchange_declare(
+        exchange = 'my_msgs',
+        type = 'direct'
+    )
+
+    listen_queue = channel.queue_declare(exclusive=True)
+    queue_name = listen_queue.method.queue
+
+    channel.queue_bind(
+        exchange = 'my_msgs',
+        queue = queue_name,
+        routing_key = "consumer1"
+    )
+
+    channel.basic_consume(on_msg_receive, queue = queue_name, no_ack = True)
     channel.start_consuming()
 
     connection.close()
