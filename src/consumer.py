5d845d6def951fcb8d1915ea038d9641841c7815
Povilas Balciunas <balciunas90@gmail.com>
2015-10-07 10:32:35 +0300

    Replace spaces with tabs for indentation

--- src/consumer.py
+++ src/consumer.py
@@ -9,47 +9,47 @@ import pika_utils
 
 
 def main():
-    connection = pika_utils.make_blocking_connection()
-    channel = connection.channel()
-    t = Thread(target=consume_message, args=(channel,))
-    t.start()
+	connection = pika_utils.make_blocking_connection()
+	channel = connection.channel()
+	t = Thread(target=consume_message, args=(channel,))
+	t.start()
 
-    time.sleep(2)
-    print "Stopping consuming"
-    channel.stop_consuming()
+	time.sleep(2)
+	print "Stopping consuming"
+	channel.stop_consuming()
 
-    print "Joining thread"
-    t.join()
-    print "Thread joined"
+	print "Joining thread"
+	t.join()
+	print "Thread joined"
 
-    channel.close()
-    connection.close()
+	channel.close()
+	connection.close()
 
 def consume_message(channel):
-    channel.exchange_declare(
-        exchange = 'my_msgs',
-        type = 'direct'
-    )
+	channel.exchange_declare(
+		exchange = 'my_msgs',
+		type = 'direct'
+	)
 
-    listen_queue = channel.queue_declare(exclusive=True)
-    queue_name = listen_queue.method.queue
+	listen_queue = channel.queue_declare(exclusive=True)
+	queue_name = listen_queue.method.queue
 
-    channel.queue_bind(
-        exchange = 'my_msgs',
-        queue = queue_name,
-        routing_key = "consumer1"
-    )
+	channel.queue_bind(
+		exchange = 'my_msgs',
+		queue = queue_name,
+		routing_key = "consumer1"
+	)
 
-    consumer_tag = channel.basic_consume(on_msg_receive, queue = queue_name, no_ack = True)
+	consumer_tag = channel.basic_consume(on_msg_receive, queue = queue_name, no_ack = True)
 
-    print "Start consuming"
-    channel.start_consuming()
-    print "Finished consuming"
+	print "Start consuming"
+	channel.start_consuming()
+	print "Finished consuming"
 
 
 def on_msg_receive(channel, method, properties, body):
-    print("Received %r" % body)
+	print("Received %r" % body)
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- src/producer.py
+++ src/producer.py
@@ -5,21 +5,21 @@ import pika_utils
 
 
 def main():
-    for i in range(20):
-        produce_message("Hello world!")
+	for i in range(20):
+		produce_message("Hello world!")
 
-    raw_input("Hit ENTER to finish.")
+	raw_input("Hit ENTER to finish.")
 
 
 def produce_message(msg):
-    connection = pika_utils.make_blocking_connection()
+	connection = pika_utils.make_blocking_connection()
 
-    channel = connection.channel()
-    channel.basic_publish(exchange = "my_msgs", routing_key = "consumer1",
-        body = msg)
+	channel = connection.channel()
+	channel.basic_publish(exchange = "my_msgs", routing_key = "consumer1",
+		body = msg)
 
-    connection.close()
+	connection.close()
 
 
 if __name__ == "__main__":
-    main()
+	main()
